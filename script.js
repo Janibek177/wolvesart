7508b73cb75140c3e0dc1c95179e9c7d9baebdd2
Janibek777 <ilmhubjanibek@gmail.com>
2026-09-20 18:39:44 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById("submit");
+submit.addEventListener("click", function(){
+    submit.textContent = "Submitting..."
+});
+
+const show = document.getElementById("show");
+show.addEventListener("click", function(){
+    show.textContent = "opening...";
+});
